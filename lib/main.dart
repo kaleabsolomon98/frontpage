f185f20d34e4aadc12e19291007ad71b644a23a4
alazar-w <ej.dalvic@gmail.com>
2021-05-16 01:02:13 +0300

    splashScreen + SplashScreenWidget, main.dart updated-> using routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project4/repository/quoteRepository.dart';
 import 'package:project4/screens/HomePage.dart';
+import 'package:project4/screens/SplashScreen.dart';
 import 'package:project4/widgets/DrawerContent.dart';
 
 import 'blocs/quoteBloc/quoteBloc.dart';
 import 'blocs/quoteBloc/quoteEvent.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  runApp(
+      BlocProvider(create: (BuildContext context) => QuoteBloc(QuoteRepository())..add(FetchQuote()),
+          child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,19 +20,14 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false ,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Teen Quote"),
-        ),
-        body: BlocProvider(
-            create: (BuildContext context) => QuoteBloc(QuoteRepository())..add(FetchQuote()),
-            child: HomePage()
-        ),
+      initialRoute: '/splash',
+      routes: {
+        '/splash':(context) => SplashScreen(),
+        '/home': (context) => HomePage(),
+//        '/details': (context) => DetailsPage(),
+      },
 
-        drawer: Drawer(
-            child:drawerContent(context)
-        ), //this will just add the Navigation Drawer  Icon
-      ),
+
     );
   }
 
---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project4/blocs/quoteBloc/quote.dart';
+import 'package:project4/screens/SplashScreen.dart';
+import 'package:project4/screens/HomePage.dart';
+import 'package:project4/widgets/SplashScreenWidget.dart';
+
+
+
+// This the widget where the BLoC states and events are handled.
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+//    final quoteBloc = BlocProvider.of<QuoteBloc>(context);
+
+    return Container(
+      height: MediaQuery
+          .of(context)
+          .size
+          .height,
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
+      color: Colors.white,
+      child: Center(
+        child: BlocBuilder<QuoteBloc, QuoteState>(
+          builder: (context, state) {
+            if (state is QuoteIsLoading) {
+              return SplashScreenWidget();
+            } else if (state is QuoteIsLoaded) {
+//               Navigator.of(context).push(MaterialPageRoute(builder:(_) => HomePage()));
+              //named
+//              Navigator.of(context).pushNamedAndRemoveUntil('/home', (Route<dynamic> route) => false);
+              //not named
+//              Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => HomePage()), (Route<dynamic> route) => false);
+              return HomePage();
+            } else if (state is QuoteIsNotLoaded) {
+              return Container(
+                child: Center(
+                  child: Text("Error Loading Data"),
+                ),
+              );
+            }
+            return Container(
+              child: Center(
+                child: Text("Default:Error Loading Data"),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
